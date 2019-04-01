ac1edc276135927858e6170b1f1b1804197dcfcc
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-01 09:43:45 -0400

    added mod blocks file

---
+++ src/main/java/danny8208/lazythings/blocks/ModBlocks.java
@@ -0,0 +1,10 @@
+package danny8208.lazythings.blocks;
+
+import danny8208.lazycore.LazyThings;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(LazyThings.MOD_ID)
+public class ModBlocks {
+    public static void registerModels() {
+    }
+}
