905312f7b10c3509fb3f83ee358a3fb6eafbc3a4
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-01 09:43:11 -0400

    changed mod id from lazycore to lazythings
    used copy and paste from lazycore

--- src/main/java/danny8208/lazythings/items/ModItems.java
+++ src/main/java/danny8208/lazythings/items/ModItems.java
@@ -1,11 +1,11 @@
 package danny8208.lazythngs.items;
 
-import danny8208.lazycore.common.LazyCore;
+import danny8208.lazycore.LazyThings;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@GameRegistry.ObjectHolder(LazyCore.MOD_ID)
+@GameRegistry.ObjectHolder(LazyThings.MOD_ID)
 public class ModItems {
     @SideOnly(Side.CLIENT)
     public static void registerModels() {
