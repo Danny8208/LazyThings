9bb04a197bc91a07ee09095aac2a4a4f3212bd18
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-01 09:55:50 -0400

    created copper pickaxe file
    might be finished have to look over

---
+++ src/main/java/danny8208/lazythings/items/PickCopper.java
@@ -0,0 +1,7 @@
+package danny8208.lazythings.items;
+
+public class PickCopper extends PickBase {
+  public PickCopper() {
+  super(LazyThings.MOD_ID, "copper_pick", LazyTab.tab, ModItems.MATERIAL_PICK_COPPER);
+  }
+}
