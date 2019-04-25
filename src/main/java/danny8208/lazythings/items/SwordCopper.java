d134f31867cc6511dc1cafa19c0061638df12650
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-25 14:35:35 -0400

    created copper sword file

---
+++ src/main/java/danny8208/lazythings/items/SwordCopper.java
@@ -0,0 +1,7 @@
+package danny8208.lazythings.items;
+
+public class SwordCopper extends SwordBase {
+  public SwordCopper() {
+    super(LazyThings.MOD_ID, "copper_sword", LazyTab.tab, ModItems.MATERIAL_SWORD_COPPER);
+  }
+}
