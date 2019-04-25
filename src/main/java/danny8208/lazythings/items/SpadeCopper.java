0362c21372ad91bb47dc50f579deffe6e4cdbfee
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-25 14:31:12 -0400

    created copper shovel file

---
+++ src/main/java/danny8208/lazythings/items/SpadeCopper.java
@@ -0,0 +1,7 @@
+package danny8208.lazythings.items;
+
+public class SpadeCopper extends SpadeBase {
+  public SpadeCopper() {
+    super(LazyThings.MOD_ID, "copper_spade", LazyTab.tab, ModItems.MATERIAL_SPADE_COPPER);
+  }
+}
