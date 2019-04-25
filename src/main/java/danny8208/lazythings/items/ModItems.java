c01d2c2db8e92c001e8a15a2428bff2e3de61d11
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-04-25 14:45:52 -0400

    added items to be registered

--- src/main/java/danny8208/lazythings/items/ModItems.java
+++ src/main/java/danny8208/lazythings/items/ModItems.java
@@ -7,7 +7,21 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @GameRegistry.ObjectHolder(LazyThings.MOD_ID)
 public class ModItems {
+    @GameRegstry.ObjectHolder("copper_pick")
+    public static PickCopper pickCopper = new PickCopper();
+    
+    @GameRegistry.ObjectHolder("copper_spade")
+    public static SpadeCopper spadeCopper = new SpadeCopper();
+    
+    @GameRegistry.ObjectHolder("copper_axe")
+    public static AxeCopper axeCopper = new AxeCopper();
+    
     @SideOnly(Side.CLIENT)
     public static void registerModels() {
+        pickCopper.registerModels();
+        
+        spadeCopper.registerModels();
+        
+        axeCopper.registerModels();
     }
 }
