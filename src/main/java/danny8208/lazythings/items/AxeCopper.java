bf1c92ae2a29775eb6bb22ee8ec002f222e506e0
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-09-11 10:18:03 -0400

    Delete AxeCopper.java

--- src/main/java/danny8208/lazythings/items/AxeCopper.java
+++
@@ -1,7 +0,0 @@
-package danny8208.lazythings.items;
-
-public class AxeCopper extends AxeBase {
-  public AxeCopper() {
-    super(LazyThings.MOD_ID, "copper_axe", LazyTab.tab, ModItems.MATERIAL_AXE_COPPER);
-  }
-}
