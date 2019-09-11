47df45b062a186a47d3e5ea23d67a0db1f024aea
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-09-11 10:17:46 -0400

    Delete SpadeCopper.java

--- src/main/java/danny8208/lazythings/items/SpadeCopper.java
+++
@@ -1,7 +0,0 @@
-package danny8208.lazythings.items;
-
-public class SpadeCopper extends SpadeBase {
-  public SpadeCopper() {
-    super(LazyThings.MOD_ID, "copper_spade", LazyTab.tab, ModItems.MATERIAL_SPADE_COPPER);
-  }
-}
