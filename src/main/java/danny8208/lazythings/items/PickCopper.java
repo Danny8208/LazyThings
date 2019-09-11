1ba596dc4ea22aa2d5d0954e9235342208ddc778
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-09-11 10:17:52 -0400

    Delete PickCopper.java

--- src/main/java/danny8208/lazythings/items/PickCopper.java
+++
@@ -1,7 +0,0 @@
-package danny8208.lazythings.items;
-
-public class PickCopper extends PickBase {
-  public PickCopper() {
-  super(LazyThings.MOD_ID, "copper_pick", LazyTab.tab, ModItems.MATERIAL_PICK_COPPER);
-  }
-}
