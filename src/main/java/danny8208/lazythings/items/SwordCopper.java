8f102816f6227d43ea05f86f17da7ee31ae312b1
Danny8208 <41124552+Danny8208@users.noreply.github.com>
2019-09-11 10:17:40 -0400

    Delete SwordCopper.java

--- src/main/java/danny8208/lazythings/items/SwordCopper.java
+++
@@ -1,7 +0,0 @@
-package danny8208.lazythings.items;
-
-public class SwordCopper extends SwordBase {
-  public SwordCopper() {
-    super(LazyThings.MOD_ID, "copper_sword", LazyTab.tab, ModItems.MATERIAL_SWORD_COPPER);
-  }
-}
